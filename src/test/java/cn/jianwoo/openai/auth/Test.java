66b35a23a05877682286da492b8acb25236ad36d
gulihua <lihua0105>
2023-02-21 12:09:23 +0800

    代码调整

--- src/test/java/cn/jianwoo/openai/auth/Test.java
+++ src/test/java/cn/jianwoo/openai/auth/Test.java
@@ -11,7 +11,7 @@ public class Test
 {
     public static void main(String[] args) throws PostException
     {
-        SessionRes accessToken = new OpenAiAuth("1729846470@qq.com", "lihua0105...").auth();
+        SessionRes accessToken = new OpenAiAuth("1729846470@qq.com", "xxxxxxx").auth();
 //        accessToken = new OpenAiAuth("1729846470@qq.com", "xxxxxxxxxx", new Proxy(Proxy.Type.HTTP,
 //                new InetSocketAddress("http://127.0.0.1", 5000))).auth();
         System.out.println(JSON.toJSONString(accessToken));
